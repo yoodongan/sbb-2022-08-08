7a240eea3451c07cc9b22ce30225dc27bc8ca321
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-08 13:55:22 +0900

    @GetMapping, @PostMapping 테스트

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,16 +1,81 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
-    @RequestMapping("/sbb")
+    static int count = -1;
+    @GetMapping("/page1")
     @ResponseBody
-    public String hello() {
-        System.out.println("Hello!!!");
-        return "Hello World!";
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이" />
+                    <input type="submit" value="page2로 POST 방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @PostMapping("/page2")     // page1 에서 form 에 값 입력 시, PostMapping 과 연결되어 page2 로 이동.
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")          // 이것은 뒤에 queryStr 붙여서 들어가야 나온다.
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, GET 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b) {
+        return a + b;
+    }
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int increaseNum() {
+        return ++count;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String showGugudan(int dan, int limit) {
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(dan, i, dan*i))
+                .collect(Collectors.joining("<br>"));
+    }
+
+    @GetMapping("mbti")
+    @ResponseBody
+    public String showMbti(@RequestParam(defaultValue = "없음") String name) {
+        if (name.equals("홍길동")) {
+            return "INFP";
+        }
+        if (name.equals("홍길순")) {
+            return "ENFP";
+        }
+        if (name.equals("임꺽정")) {
+            return "INFJ";
+        }
+        return null;
     }
 
 }
