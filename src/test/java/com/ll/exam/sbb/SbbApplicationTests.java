a6d743178c99d63543b4c35ccf7c3056680f96d7
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-10 14:38:19 +0900

    조회 시 콘솔 창에 쿼리문 띄우기, findBySubjectAndContent 테스트 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -52,4 +52,11 @@ class SbbApplicationTests {
 		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertEquals(1, q.getId());
 	}
+
+	@Test
+	void findBySubjectAndContent() {
+		Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+
 }
