2faf5e2d86f03ba10e5b4dacdf1e6e904c72bc02
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-10 16:39:13 +0900

    수정과 삭제 테스트

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -64,6 +66,25 @@ class SbbApplicationTests {
 		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void modifyQuestion() {
+		Optional<Question> oq = questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("제목 수정");
+		questionRepository.save(q);
+	}
+	@Test
+	void deleteQuestion() {
+		assertEquals(2, questionRepository.count());
+		Optional<Question> oq = questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		questionRepository.delete(q);
+		assertEquals(1, questionRepository.count());
 	}
 
 
