91da5866d7305da1d7e94d062c6fd530382c9a89
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-12 11:40:52 +0900

    AnswerRepositoryTest 조회 기능 구현.

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class AnswerRepositoryTest {
 
@@ -21,29 +23,27 @@ class AnswerRepositoryTest {
         createSampleData();
     }
 
-
     void clearTest() {
-        questionRepository.truncateTable();
-
-        answerRepository.disableForeignKeyCheck();
-        answerRepository.enableForeignKeyCheck();
+        QuestionRepositoryTest.clearData(questionRepository);
         answerRepository.truncateTable();
-
     }
 
     void createSampleData() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q1);
+        QuestionRepositoryTest.createSampleData(questionRepository);
 
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q2);
+        Question q = questionRepository.findById(1).get();
 
+        Answer a1 = new Answer();
+        a1.setContent("sbb는 질문답변 게시판 입니다.");
+        a1.setQuestion(q);
+        a1.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
+        a2.setQuestion(q);
+        a2.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a2);
     }
 
     @Test
@@ -56,4 +56,10 @@ class AnswerRepositoryTest {
         a.setCreateDate(LocalDateTime.now());
         answerRepository.save(a);
     }
+    @Test
+    void 조회() {
+        Answer a = this.answerRepository.findById(1).get();
+        assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
+    }
+
 }
