488317afae88815cebe8dbcbb59fb3aaaf6f9c49
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-15 23:00:16 +0900

    타임리프 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,21 +1,23 @@
 package com.ll.exam.sbb.question;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+
 @Controller
-public class QuestionController
-{
-    @Autowired
-    private QuestionService questionService;
+@RequiredArgsConstructor
+public class QuestionController {
+    private final QuestionRepository questionRepository;
 
-    @RequestMapping("list")
-    @ResponseBody
-    public String showList() {
-        questionService.findById(1);
-
-        return "HI";
+    @RequestMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);  // "questionList" 라는 이름으로 questionList 변수를 보내는 것.
+        return "question_list";
     }
 }
