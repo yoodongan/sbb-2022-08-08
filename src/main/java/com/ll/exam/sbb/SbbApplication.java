451c33a53c5b8bd8a5d89b018024fcb53f0b42a4
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-15 23:19:32 +0900

    new branch : practice1

--- src/main/java/com/ll/exam/sbb/SbbApplication.java
+++ src/main/java/com/ll/exam/sbb/SbbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbbApplication {
-
+	// new branch : practice1 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(SbbApplication.class, args);
 	}
