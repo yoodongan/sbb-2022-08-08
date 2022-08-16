d52a8646f47ce8e1b1c043eb90744c5c9d5dfd84
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-16 10:12:08 +0900

    test3

---
+++ src/main/java/com/ll/exam/sbb/controller/MainController.java
@@ -0,0 +1,54 @@
+package com.ll.exam.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    private static int increasingNumber = -1;
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showFormPage() {
+        return """
+                <form method = "POST" action="/page2">
+                  <input type="text" name = "name" placeholder ="이름을 입력해주세요."/>
+                  <input type="submit" value = "page2 로 POST 이동"/>
+                </form>
+                """;
+    }
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "이름 없음") String d) {
+        return """
+                <h1>입력된 이름 : %s</h1>
+                <h1>안녕하세요 %s님, POST 방식으로 오셨군요.</h1>
+                """.formatted(d, d);
+    }
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") String name) {
+        return """
+                <h1>입력된 이름 : %s</h1>
+                <h1>안녕하세요 %s님, POST 방식으로 오셨군요.</h1>
+                """.formatted(name, name);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int increaseNum() {
+        return ++increasingNumber;
+    }
+
+
+
+}
