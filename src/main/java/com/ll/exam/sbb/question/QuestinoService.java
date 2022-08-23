0f43f1f31b30885c879470937803a2392a0100f7
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-23 18:31:58 +0900

    질문 상세

--- src/main/java/com/ll/exam/sbb/question/QuestinoService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestinoService.java
@@ -1,9 +1,11 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,14 @@ public class QuestinoService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> oq = questionRepository.findById(id);
+        if (oq.isPresent()) {
+            return oq.get();
+        } else {
+            throw new DataNotFoundException("question이 존재하지 않습니다!");
+        }
     }
 }
