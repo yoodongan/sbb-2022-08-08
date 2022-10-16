73e25231fc3d508047ed2509b9061e25df5167eb
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-10-16 15:30:40 +0900

    2-11. URL 프리픽스
    - 공통 URL 처리

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -5,27 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")     // 질문 목록 페이지
+    @GetMapping("/list")     // 질문 목록 페이지
     public String showList(Model model) {
         List<Question> questionList = questionService.findAll();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/")   // root url
-    public String root(Model model) {
-        return "redirect:/question/list";
-    }
-
-    @GetMapping("/question/detail/{questionId}")
+    @GetMapping("/detail/{questionId}")
     public String showDetail(Model model, @PathVariable Long questionId) {
         Question question = questionService.findById(questionId);
         model.addAttribute("question", question);
