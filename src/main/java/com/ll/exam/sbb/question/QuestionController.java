cf061a2e86a7c33e40f32623c39a186100071327
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-10-16 15:28:43 +0900

    2-10. 질문 상세
    - 질문 상세 링크, 확인하기
    - 질문 없을 시, 예외처리 클래스 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -12,15 +13,24 @@ import java.util.List;
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
-    public String list(Model model) {
+    @GetMapping("/question/list")     // 질문 목록 페이지
+    public String showList(Model model) {
         List<Question> questionList = questionService.findAll();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/")
+    @GetMapping("/")   // root url
     public String root(Model model) {
         return "redirect:/question/list";
     }
+
+    @GetMapping("/question/detail/{questionId}")
+    public String showDetail(Model model, @PathVariable Long questionId) {
+        Question question = questionService.findById(questionId);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+
+
 }
--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataLocationNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +17,9 @@ public class QuestionService {
         return questionRepository.findAll();
     }
 
+    public Question findById(Long questionId) {
+        Optional<Question> oQuestion = questionRepository.findById(questionId);
+        if(oQuestion.isEmpty()) throw new DataNotFoundException("question not found");
+        return oQuestion.get();
+    }
 }
